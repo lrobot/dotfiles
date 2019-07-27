8b52d6c0d4978c0fcb3d685e663ee834b0f9ebc8
lrobot <lrobot.qq@gmail.com>
2019-07-27 17:28:33 +0800

    m

---
+++ 00my/install_ubuntu.sh
@@ -0,0 +1,10 @@
+
+
+apt install software-properties-common
+apt install git tmux lazygit
+apt install nmap curl wget diffutils sed awk
+apt install clang-format clang clang-tools cmake cscope exuberant-ctags autoconf cmake-curses-gui global gdb
+#autoconf  has "autoreconf"
+apt install python2.7-dev
+apt install libssl-dev
+
